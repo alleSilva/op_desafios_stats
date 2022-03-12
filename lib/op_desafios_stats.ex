78cf4f740aa17d615987be0904428029eae05a80
Alesandro Silva <silva_alesandro@outlook.com>
2022-03-12 18:00:46 -0400

    initial commit

---
+++ lib/op_desafios_stats.ex
@@ -0,0 +1,30 @@
+defmodule OpDesafiosStats do
+  def call() do
+    File.read!("desafios.txt")
+    |> String.split()
+    |> Enum.map(& separate(&1))
+    |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, List.last(x), 1, &(&1 + 1)) end)
+  end
+
+  def separate(line) do
+    line
+    |> String.trim("./")
+    |> String.split("/")
+    |> List.update_at(2, &cleanFeature(&1))
+  end
+
+  defp cleanFeature(lang_feature) do
+    x = String.split(lang_feature, "-")
+    get_lang(x)
+  end
+
+  defp get_lang([lang]) do
+    if (lang == "js" || lang == "nodejs" || lang == "JS") do
+      "javascript"
+    else
+      lang
+    end
+  end
+
+  defp get_lang([lang | _]), do: get_lang([lang])
+end
---
+++ test/op_desafios_stats_test.exs
@@ -0,0 +1,8 @@
+defmodule OpDesafiosStatsTest do
+  use ExUnit.Case
+  doctest OpDesafiosStats
+
+  test "greets the world" do
+    assert OpDesafiosStats.hello() == :world
+  end
+end
